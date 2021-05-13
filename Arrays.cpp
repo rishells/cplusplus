3fb63fcd3e3f054fc24dadce6b1caab24a8a213f
unknown <rishells@ricardoconchas@gmail.com>
2021-05-13 12:01:10 -0500

    Added practice to the Arrays file

--- Arrays.cpp
+++ Arrays.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main()
 {
-    cout <<"hello";
+    int n = 0;
+    // cout<<"Enter Size:"<<endl;
+    // cin>>n;
+    int A[] = {1,2,3,4,5,6,7,8,9};
+    cout << "size of Array is: " << sizeof(A) /sizeof(int)<<endl;
+    cout<< A[0] <<endl;
+    cout<< A[1] <<endl;
+    cout<< A[2] <<endl;
+    cout<< A[3] <<endl;
+    printf("%i\n", A[4]);
+    printf("%i", A[5]);
+
+    // for(int i=0;i<n;i++){
+    //     cout<<A[n]<<endl;
+    // }
+    
     return 0;
 }
